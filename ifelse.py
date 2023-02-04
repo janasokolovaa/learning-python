f41d94158b3a8778cc0f4dd1a0d5a96ca3c2914a
janasokolovaa <123120704+janasokolovaa@users.noreply.github.com>
2023-02-04 22:50:17 +0100

    ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,30 @@
+# learning and practising if/else statements
+
+
+my_money = 200
+# If I have more than 2000, print 'I am rich' else 'I am poor'
+if my_money > 2000:
+    print('I am rich')
+elif my_money == 200:
+    print('I am doing ok')
+else:
+    print('I am poor')
+
+
+
+dad = 40
+mom = 42
+# If my dad is older than my mom, print my dad is the oldest, else print mom is the oldest
+if dad > mom:
+    print('My dad is the oldest')
+else:
+    print('My mum is the oldest')
+
+
+
+noise_level = 200
+danger_message = "DANGER!"
+# if noise level is greater than 100, print 'The message is: {danger message}'
+if noise_level >= 100:
+    print('The message is', danger_message)
+
